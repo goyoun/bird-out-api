df84d4755f896a4c61b90f2c6b446789e5f2f9b7
superpil-itech <97083849+superpil-itech@users.noreply.github.com>
2022-04-12 16:56:53 +0900

    init

---
+++ src/main/java/api/birdout/BirdOutApplication.java
@@ -0,0 +1,13 @@
+package api.birdout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BirdOutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BirdOutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/birdout/BirdOutApplicationTests.java
@@ -0,0 +1,13 @@
+package api.birdout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BirdOutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
