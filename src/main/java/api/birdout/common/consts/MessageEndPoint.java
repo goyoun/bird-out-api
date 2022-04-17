41dc99c63900542412bac68a488f25ec08cf5f01
pygmalion0909 <peel0220@gmail.com>
2022-04-17 13:08:00 +0000

    fix: file delete

--- src/main/java/api/birdout/common/consts/MessageEndPoint.java
+++
@@ -1,8 +0,0 @@
-package api.birdout.common.consts;
-
-public class MessageEndPoint {
-  
-  public static final String SIGNIN = "/isms/api/v1/auth/signin";
-  public static final String SMS_MESSAGE = "/isms/api/v1/sms";
-
-}
