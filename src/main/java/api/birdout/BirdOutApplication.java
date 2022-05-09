be1f33b622972667a0a48cd181a0c567f59103ab
superpil-itech <97083849+superpil-itech@users.noreply.github.com>
2022-05-09 17:25:14 +0900

    fix: for merge

--- src/main/java/api/birdout/BirdOutApplication.java
+++ src/main/java/api/birdout/BirdOutApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BirdOutApplication {
-	String aa = "git test";
 	public static void main(String[] args) {
 		SpringApplication.run(BirdOutApplication.class, args);
 	}
-
 }
